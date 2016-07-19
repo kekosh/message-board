073c99c80afddfe0be9de5fd21415d92ff8afb9f
kekosh <kenta0540@gmail.com>
2016-07-19 22:47:27 +0000

    crate message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
+  #モデルオブジェクトを作成
   def index
+    @message = Message.new
+    @messages = Message.all
+  end
+  
+  def create
+    @message = Message.new(messages_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました。'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name,:body)
   end
 end
