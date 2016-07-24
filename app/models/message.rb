96f4b1e3de2493ad89fc703fbf4ff066f9ff6e25
kekosh <kenta0540@gmail.com>
2016-07-24 14:43:49 +0000

    challenge2 complete.

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,7 @@ class Message < ActiveRecord::Base
     
     #内容は必須入力かつ２文字以上30文字以下
     validates :body, length:{minimum:2,maximum:30}, presence: true
+    
+    #年齢0以上の数値のみ
+    validates :age, numericality:{greater_than_or_equal_to:0,less_than:120,only_integer:true}, presence: true
 end
